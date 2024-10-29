c6b81a27594d0d6de2d1b3dbc67d0d288704233a
Amir Hesam <113293761+Hesam-mhm@users.noreply.github.com>
2024-10-29 17:13:00 +0330

    first commit

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -63,63 +63,121 @@ const Home = () => {
     const value = monitoringData.f ? monitoringData.f : 0
 
     let color = ''
+    let color2 = ''
 
     if (value === 0) {
       color = '#FF0000'
+      color2 = 'rgba(256,256,256,0.53)'
     } else {
-      color = '#e2e2e2'
+      color = '#green'
+      color2 = 'rgba(256,256,256,0.00)'
     }
 
-    const motor = svg.getElementById('_99-2') as SVGGraphicsElement
-    console.log(motor)
+    const motor = svg.querySelector('.cls-8') as SVGGraphicsElement
+    const motorrrr = svg.querySelector('.cls-9') as SVGGraphicsElement
 
-    if (motor) {
-      // motor.setAttribute('stroke', color)
-      // motor.setAttribute('stroke-width', color)
-      if (motor.tagName === 'rect' || motor.tagName === 'circle' || motor.tagName === 'path') {
-        motor.setAttribute('fill', color)
-      }
-    }
+    console.log(motorrrr)
+
+    // motor.setAttribute('stroke', color)
+    // motor.setAttribute('stroke-width', color)
+    motor.setAttribute('style', `fill: ${color}; stroke-width: 2;`)
+    motorrrr.setAttribute('style', `fill: ${color2}; stroke-width: 2;`)
   }
 
   const changeInputValue5 = (svg: SVGSVGElement) => {
     const value = monitoringData.e ? monitoringData.e : 0
-
+    let textCol
+    if (monitoringData.f === 0) {
+      textCol = '#E8E8E8'
+    } else {
+      textCol = '#1c1c1c'
+    }
     const text = svg.getElementById('_5')
+    const textBg = svg.getElementById(`_5_1`) as HTMLElement
+
     if (text) {
-      text.textContent = value.toString()
+      text.textContent = `${value.toString()}rpm`
+      text.setAttribute('fill', textCol) // Replace "desiredColor" with the color you want, e.g., "#FF0000" for red
+
+      // textBg.style.fill = '#FF0000'
     }
   }
+
   const changeInputValue4 = (svg: SVGSVGElement) => {
     const value = monitoringData.d ? monitoringData.d : 0
-
+    let textCol
+    if (monitoringData.f === 0) {
+      textCol = '#E8E8E8'
+    } else {
+      textCol = '#1c1c1c'
+    }
     const text = svg.getElementById('_4')
+    const textBg = svg.getElementById(`_4_1`) as HTMLElement
+
     if (text) {
-      text.textContent = value.toString()
+      text.textContent = `${value.toString()}°C`
+      text.setAttribute('fill', textCol) // Replace "desiredColor" with the color you want, e.g., "#FF0000" for red
+
+      // textBg.style.fill = '#FF0000'
     }
   }
+
   const changeInputValue3 = (svg: SVGSVGElement) => {
     const value = monitoringData.c ? monitoringData.c : 0
-
+    let textCol
+    if (monitoringData.f === 0) {
+      textCol = '#E8E8E8'
+    } else {
+      textCol = '#1c1c1c'
+    }
     const text = svg.getElementById('_3')
+    const textBg = svg.getElementById(`_3_1`) as HTMLElement
+
     if (text) {
-      text.textContent = value.toString()
+      text.textContent = `${value.toString()}°C`
+      text.setAttribute('fill', textCol) // Replace "desiredColor" with the color you want, e.g., "#FF0000" for red
+
+      textBg.style.fill = '#FF0000'
+      textBg.style.fill = value > 200 && monitoringData.f === 1 ? '#FF0000' : '#E8E8E8'
     }
   }
+
   const changeInputValue2 = (svg: SVGSVGElement) => {
     const value = monitoringData.b ? monitoringData.b : 0
+    let textCol
+    if (monitoringData.f === 0) {
+      textCol = '#E8E8E8'
+    } else {
+      textCol = '#1c1c1c'
+    }
 
     const text = svg.getElementById('_2')
+    const textBg = svg.getElementById(`_2_1`) as HTMLElement
+
     if (text) {
-      text.textContent = value.toString()
+      text.textContent = `${value.toString()}kg/h`
+      text.setAttribute('fill', textCol) // Replace "desiredColor" with the color you want, e.g., "#FF0000" for red
+
+      // textBg.style.fill = '#FF0000'
     }
   }
+
   const changeInputValue1 = (svg: SVGSVGElement) => {
     const value = monitoringData.a ? monitoringData.a : 0
-
+    let textCol
+    if (monitoringData.f === 0) {
+      textCol = '#E8E8E8'
+    } else {
+      textCol = '#1c1c1c'
+    }
     const text = svg.getElementById('_50')
+    text.setAttribute('fill', textCol) // Replace "desiredColor" with the color you want, e.g., "#FF0000" for red
+
+    // const textBg = svg.getElementById(`_50_1`) as HTMLElement
+
     if (text) {
-      text.textContent = value.toString()
+      text.textContent = `${value.toString()}rpm`
+      // textBg.style.fill = '#FF0000'
     }
   }
 
@@ -158,7 +216,7 @@ const Home = () => {
             svg.removeAttribute('width')
             svg.removeAttribute('height')
           }}
-          src='/textFieldss.svg' // Reference without `public`
+          src='/SVG - OpacityRect- v3.svg' // Reference without `public`
           title='Title'
           useRequestCache={true}
           wrapper='div'
